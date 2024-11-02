58033c3129969133dcce0ab795a557138f6aad25
tkibnyusuf <85669533+tkibnyusuf@users.noreply.github.com>
2024-11-02 12:29:20 -0500

    Update install_jenkins.sh

--- JenkinsTFscript/install_jenkins.sh
+++ JenkinsTFscript/install_jenkins.sh
@@ -11,6 +11,17 @@ sudo apt install maven -y
 sudo apt update
 sudo apt install jenkins -y
 sudo apt-get update -y
+wget -O- https://apt.releases.hashicorp.com/gpg | \
+    gpg --dearmor | \
+    sudo tee /usr/share/keyrings/hashicorp-archive-keyring.gpg
+gpg --no-default-keyring \
+    --keyring /usr/share/keyrings/hashicorp-archive-keyring.gpg \
+    --fingerprint
+echo "deb [signed-by=/usr/share/keyrings/hashicorp-archive-keyring.gpg] \
+    https://apt.releases.hashicorp.com $(lsb_release -cs) main" | \
+    sudo tee /etc/apt/sources.list.d/hashicorp.list
+sudo apt update
+sudo apt install terraform -y
 echo 'clearing screen...' && sleep 5
 clear
 echo 'jenkins is installed'
