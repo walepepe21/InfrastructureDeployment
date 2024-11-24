116da0e5962036a6f1912c0503b2282a1fa77e5d
walepepe21 <125354015+walepepe21@users.noreply.github.com>
2024-11-24 02:04:58 -0800

    Update install_jenkins.sh

--- JenkinsTFscript/install_jenkins.sh
+++ JenkinsTFscript/install_jenkins.sh
@@ -1,15 +1,27 @@
 #!/bin/sh
 sudo apt update
-sudo apt install openjdk-17-jre-headless -y
 sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 63667EE74BBA1F0A08A698725BA31D57EF5975CA
 wget -q -O - https://pkg.jenkins.io/debian-stable/jenkins.io.key | sudo apt-key add -
 sudo sh -c 'echo deb http://pkg.jenkins.io/debian-stable binary/ > /etc/apt/sources.list.d/jenkins.list'
+sudo apt update
+sudo apt install openjdk-17-jre-headless -y
 sudo apt install ca-certificates
-sudo apt update -y
-sudo sh -c 'echo deb http://pkg.jenkins.io/debian-stable binary/ > /etc/apt/sources.list.d/jenkins.list'
+sudo apt update
+sudo apt install maven -y
 sudo apt update
 sudo apt install jenkins -y
 sudo apt-get update -y
+wget -O- https://apt.releases.hashicorp.com/gpg | \
+    gpg --dearmor | \
+    sudo tee /usr/share/keyrings/hashicorp-archive-keyring.gpg
+gpg --no-default-keyring \
+    --keyring /usr/share/keyrings/hashicorp-archive-keyring.gpg \
+    --fingerprint
+echo "deb [signed-by=/usr/share/keyrings/hashicorp-archive-keyring.gpg] \
+    https://apt.releases.hashicorp.com $(lsb_release -cs) main" | \
+    sudo tee /etc/apt/sources.list.d/hashicorp.list
+sudo apt update
+sudo apt install terraform -y
 echo 'clearing screen...' && sleep 5
 clear
 echo 'jenkins is installed'
