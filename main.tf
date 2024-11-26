49e7704421e8764d36bdd892c1baf6622c525ac4
walepepe21 <125354015+walepepe21@users.noreply.github.com>
2024-11-25 21:54:25 -0800

    Update main.tf

--- main.tf
+++ main.tf
@@ -4,7 +4,7 @@ provider "aws" {
 
 terraform {
   backend "s3" {
-    bucket = "yusuf-jenkins"
+    bucket = "pepe-jenkins"
     key    = "newresource.tfstate"
     region = "us-east-1"
   }
@@ -103,7 +103,7 @@ resource "aws_instance" "foo" {
   instance_type = "t2.micro"
   subnet_id = aws_subnet.demo_subnet.id
   vpc_security_group_ids = [aws_security_group.allow_tls.id]
-  availability_zone = "us-east-1a"
+  availability_zone = "us-east-1"
   key_name = "devopskeypair"
   count = 5
 
