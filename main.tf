84a0e62a0ecf07931b062137319f5da2d760b194
walepepe21 <125354015+walepepe21@users.noreply.github.com>
2024-11-25 23:59:54 -0800

    Update main.tf

--- main.tf
+++ main.tf
@@ -103,7 +103,7 @@ resource "aws_instance" "foo" {
   instance_type = "t2.micro"
   subnet_id = aws_subnet.demo_subnet.id
   vpc_security_group_ids = [aws_security_group.allow_tls.id]
-  availability_zone = "us-east-1"
+  availability_zone = "us-east-1a"
   key_name = "devopskeypair"
   count = 5
 
